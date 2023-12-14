9c512c82937019a922abd639199574fed64fda09
TulioH <t.hernandez@abracol.com>
2023-12-14 09:42:44 -0500

    se cambia los servicios para que se registren usando hashicorp consul

--- movie/cmd/main.go
+++ movie/cmd/main.go
@@ -1,23 +1,53 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/thernande/movie-micro/movie/internal/controller/movie"
 	metadatagateway "github.com/thernande/movie-micro/movie/internal/gateway/metadata/http"
 	ratinggateway "github.com/thernande/movie-micro/movie/internal/gateway/rating/http"
 	httphandler "github.com/thernande/movie-micro/movie/internal/handler/http"
+	"github.com/thernande/movie-micro/pkg/discovery"
+	"github.com/thernande/movie-micro/pkg/discovery/consul"
 )
 
+const serviceName = "movie"
+
 func main() {
-	log.Println("Starting the movie service")
-	metadataGateway := metadatagateway.New("localhost:8081")
-	ratingGateway := ratinggateway.New("localhost:8082")
-	ctrl := movie.New(ratingGateway, metadataGateway)
-	h := httphandler.New(ctrl)
+	var port int
+	flag.IntVar(&port, "port", 8083, "API handler port")
+	flag.Parse()
+	log.Printf("Starting the movie service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.
+				ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
+	metadataGateway := metadatagateway.New(registry)
+	ratingGateway := ratinggateway.New(registry)
+	svc := movie.New(ratingGateway, metadataGateway)
+	h := httphandler.New(svc)
 	http.Handle("/movie", http.HandlerFunc(h.GetMovieDetails))
-	if err := http.ListenAndServe(":8083", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		panic(err)
 	}
 }
---
+++ pkg/discovery/consul/consul.go
@@ -0,0 +1,75 @@
+package consul
+
+import (
+	"context"
+	"errors"
+	"strconv"
+	"strings"
+
+	consul "github.com/hashicorp/consul/api"
+	"github.com/thernande/movie-micro/pkg/discovery"
+)
+
+// Registry defines a service registry.
+type Registry struct {
+	client *consul.Client
+}
+
+// NewRegistry creates a new Consul service registry instance.
+func NewRegistry(addr string) (*Registry, error) {
+	config := consul.DefaultConfig()
+	config.Address = addr
+	client, err := consul.NewClient(config)
+	if err != nil {
+		return nil, err
+	}
+	return &Registry{client: client}, nil
+}
+
+// Register creates a service instance record in the registry.
+func (r *Registry) Register(ctx context.Context, instanceID string, serviceName string, hostPort string) error {
+	parts := strings.Split(hostPort, ":")
+	if len(parts) != 2 {
+		return errors.New("hostPort must be in a form of <host>:<port>, example: localhost:8081")
+	}
+	port, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return err
+	}
+	return r.client.Agent().ServiceRegister(&consul.AgentServiceRegistration{
+		ID:      instanceID,
+		Name:    serviceName,
+		Tags:    []string{hostPort},
+		Port:    port,
+		Address: parts[0],
+		Check: &consul.AgentServiceCheck{
+			TTL:     "5s",
+			CheckID: instanceID,
+		},
+	})
+}
+
+// Deregister removes a service insttance record from the registry.
+func (r *Registry) Deregister(ctx context.Context, instanceID string, _ string) error {
+	return r.client.Agent().ServiceDeregister(instanceID)
+}
+
+// ServiceAddresses returns the list of addresses of active instances of the given service.
+func (r *Registry) ServiceAddresses(ctx context.Context, serviceName string) ([]string, error) {
+	var addresses []string
+	services, _, err := r.client.Health().Service(serviceName, "", true, &consul.QueryOptions{})
+	if err != nil {
+		return nil, err
+	} else if len(services) == 0 {
+		return nil, discovery.ErrNotFound
+	}
+	for _, service := range services {
+		addresses = append(addresses, service.Checks[0].Output)
+	}
+	return addresses, nil
+}
+
+// ReportHealthyState is a push mechanism for reporting healthy state to the registry.
+func (r *Registry) ReportHealthyState(instanceID string, serviceName string) error {
+	return r.client.Agent().PassTTL(instanceID, "")
+}
